9445b0666e807f12e7c3c2d550f44188d1648ee5
Wascar Perez <wascar.perez@yahoo.com>
2025-02-24 18:18:53 -0500

    initial commit: Created spring boot project

---
+++ src/main/java/com/example/openfin_api/OpenfinApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.openfin_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenfinApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenfinApiApplication.class, args);
+	}
+
+}
